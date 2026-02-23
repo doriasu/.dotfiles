f764e909a724ec29b9e89e020d5c874616c2ead7
orukt <orukt@pixiv.co.jp>
2026-02-23 22:52:28 +0900

    telescope fallback if not git directory

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,18 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { mode = "n", "<leader>f", "<cmd>Telescope git_files<CR>", desc = "gitで管理されているfileをfindする" },
+    {
+      mode = "n",
+      "<leader>f",
+      function()
+        local builtin = require("telescope.builtin")
+        local ok = pcall(builtin.git_files, { show_untracked = true })
+        if not ok then
+          builtin.find_files()
+        end
+      end,
+      desc = "git管理ならgit_files、非管理ならfind_files"
+    },
     { mode = "n", "<leader>g", "<cmd>Telescope live_grep<CR>", desc = "文字列検索" }
   },
   config = function()
