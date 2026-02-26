f01b9c0b82346505e3a05760d7eff3f185bd1739
orukt <orukt@pixiv.co.jp>
2026-02-26 10:27:14 +0900

    change terminal keybind

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -7,4 +7,4 @@ vim.api.nvim_set_keymap('n','<leader>h','<cmd>bprev<CR>',{})
 vim.api.nvim_set_keymap('n','<leader>l','<cmd>bnext<CR>',{})
 vim.api.nvim_set_keymap('n','<leader>x','<cmd>bdelete<CR>',{})
 -- terminal mode
-vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], {noremap = true})
+vim.api.nvim_set_keymap('t', '<Esc><Esc>', [[<C-\><C-n>]], {noremap = true})
