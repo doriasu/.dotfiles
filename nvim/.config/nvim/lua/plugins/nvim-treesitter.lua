927b086a6931f1048f9befe916272fee67e1d892
orukt <orukt@pixiv.co.jp>
2026-07-15 18:55:34 +0900

    fix nvim-treesitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter').setup({
+    require('nvim-treesitter.configs').setup({
       ensure_installed = {
         'ruby',
         'lua',
